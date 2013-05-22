364ec38b85480dff9fc7e2eddd7019f04312cdd7
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2013-05-22 12:02:21 -0400

    Add in timezone setting and comment this a little better.

--- recipes/ubuntu_base.rb
+++ recipes/ubuntu_base.rb
@@ -9,8 +9,21 @@
 
 # This includes all the standard linux utilities
 # for an ubuntu box.
+
+# We need apt on ubuntu
 include_recipe "apt"
+
+# Here are some networking tools, including
+# iptables, wget, curl, etc...
 include_recipe "networking_basic"
+
+# Let's build in security features
 include_recipe "chef-dsdev-recipes::security_basic"
+
+# We want to set the timezone and time properly.
+node['tz'] = 'America/New_York'
+include_recipe "timezone-ii::debian"
 include_recipe "ntp"
+
+# Everyone needs vim.
 include_recipe "vim"
